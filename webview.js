7976e5c96d725792f1fb0782d8d01771bd99f810
Jason Cox <jcox@resdat.com>
2018-05-04 16:14:21 -0700

    Update to include skype

--- webview.js
+++ webview.js
@@ -1,7 +1,16 @@
 module.exports = (Franz) => {
     const getMessages = function getMessages() {
         const unreadMail = jQuery("span[title*='Inbox'] + div > span").first().text();
-        Franz.setBadge(unreadMail);
+        const unreadSkype = jQuery('#O365_Lync_ButtonID > div > span:nth-child(1)').first().text();
+        
+        let cnt = 0;
+        if (unreadMail) {
+             cnt +=  Number.parseInt(unreadMail);
+        }
+        if (unreadSkype) {
+             cnt +=  Number.parseInt(unreadSkype);
+        }
+        Franz.setBadge(cnt);
     };
     Franz.loop(getMessages);
 };
